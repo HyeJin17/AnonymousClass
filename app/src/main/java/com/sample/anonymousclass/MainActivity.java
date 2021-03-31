64074f940e2609fc071a50bb6d1be03a4e44d824
HyeJin <rkd2313@gmail.com>
2021-03-31 14:08:15 +0900

    Anonymous class for multiple buttons

--- app/src/main/java/com/sample/anonymousclass/MainActivity.java
+++ app/src/main/java/com/sample/anonymousclass/MainActivity.java
@@ -7,8 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    Button mButton1;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button mButton1, mButton2;
     TextView mTextView1;
 
     @Override
@@ -17,14 +17,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mButton1 = findViewById(R.id.button1);
+        mButton2 = findViewById(R.id.button2);
         mTextView1 = findViewById(R.id.textView1);
 
-        mButton1.setOnClickListener(new View.OnClickListener(){
+        mButton1.setOnClickListener(this);
+        mButton2.setOnClickListener(this);
+    }
 
-            @Override
-            public void onClick(View v) {
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button1:
                 mTextView1.setText("강혜진의 버튼을 클릭했습니다!");
-            }
-        });
+                break;
+            case R.id.button2:
+                mTextView1.setText("강혜진의 2nd 버튼을 클릭했습니다!");
+                break;
+            default:
+                break;
+        }
     }
 }
